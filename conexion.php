2d848c695dab2e1ce2ed78f0e21c260931615bfb
crisgdl13-dotcom <crisgdl13@gmail.com>
2025-12-17 14:09:04 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -5,4 +5,10 @@ $host = "localhost";
 $usuario = "root";
 $password = "Abcdef12345#";
 $basededatos = "cursos_online_db";
+
+$conexion = new mysqli($host, $usuario, $password, $basededatos);
+
+if ($conexion->connect_error) {
+    die("Error de conexión: " . $conexion->connect_error);
+}
 ?>
